92dbcf8b6b748927906c98e3c41d5c582419ae01
Fredrik Thulin <fredrik@thulin.net>
2019-10-21 11:25:45 +0200

    fix sync with upstream

--- manifests/update.pp
+++ manifests/update.pp
@@ -57,13 +57,12 @@ class apt::update {
     $_refresh = true
   }
   exec { 'apt_update':
-    command     => "${apt::params::provider} update",
+    command     => "${::apt::provider} update",
     loglevel    => $::apt::_update['loglevel'],
     logoutput   => 'on_failure',
-    refreshonly => true,
-    timeout     => $apt::update_timeout,
-    tries       => $apt::update_tries,
-    try_sleep   => 1,
-    returns     => [0,100]
+    refreshonly => $_refresh,
+    timeout     => $::apt::_update['timeout'],
+    tries       => $::apt::_update['tries'],
+    try_sleep   => 1
   }
 }
